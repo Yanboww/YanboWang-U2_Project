bc12a6fc66566477d428a7ac7311aeb5137c1292
Yanboww <wywkaixin10@gmail.com>
2023-10-04 16:46:35 -0400

    Fixed up Design Document, added new code in EquationRunner and debugged LinearEquation class

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -8,13 +8,20 @@ public class EquationRunner {
         //use the string coord1 to get its  coordinate substrings using indexOf and length method
         int x1 = Integer.parseInt(coord1.substring(1,coord1.indexOf(",")));
         int y1 = Integer.parseInt(coord1.substring(coord1.indexOf(",")+1,coord1.length()-1));
-        System.out.print("Enter your second cooridnate point: ");
+        System.out.print("Enter your second coordinate point: ");
         String coord2 = s.nextLine();
         int x2 = Integer.parseInt(coord2.substring(1,coord2.indexOf(",")));
         int y2 = Integer.parseInt(coord2.substring(coord2.indexOf(",")+1,coord2.length()-1));
         //creates LinearEquation object
         LinearEquation idk = new LinearEquation(x1,y1,x2,y2,coord1,coord2);
+        //prints out info
         System.out.println(idk);
+        //asks for the last value
+        System.out.print("Enter a third x value: ");
+        String transX3 = s.nextLine();
+        //parse string into double and then use the value in calcY method in a string
+        double x3 = Double.parseDouble(transX3);
+        System.out.println("Solve coordinate point is: " + idk.calcY(x3));
 
 
 
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,4 +1,5 @@
 public class LinearEquation {
+    //instance variables
     private int x1;
     private int y1;
     private int x2;
@@ -10,7 +11,7 @@ public class LinearEquation {
     private String formula;
     private double distance;
 
-
+    //one constructor to set up the essential information
     public LinearEquation(int x1, int y1, int x2, int y2,String coord1, String coord2){
         this.x1 = x1;
         this.y1 = y1;
@@ -21,42 +22,48 @@ public class LinearEquation {
     }
 
 
-
+//finds the slope of the coordinates using the slope formula and round using the Math class
     public double findSlope()
     {
-        slope = Math.round((double)(y1-y2)/(x1-x2)*100)/100;
-        this.slope = slope;
+        slope = Math.round((double)(y1-y2)/(x1-x2)*100.0)/100.0;
         return slope;
 
     }
 
+    //gets the y-intercept by rewriting the equation and round using Math class
     public double getYInt()
     {
-        yInt  = Math.round((y2 - x2*slope)*100)/100;
+        yInt  = Math.round((y2 - x2*slope)*100.0)/100.0;
         return yInt;
     }
 
+    //calculates the numerator and denominator then concatenate them with other instance variables
     public String findFormula()
-    {   double top = y2-y1;
-        double bottom = x2-x1;
+    {   int top = y2-y1;
+        int bottom = x2-x1;
         formula = "y = " + top + "/" + bottom + "x + " + yInt;
         return formula;
     }
-
+    //use the distance formula and Math methods before rounding
     public double getDistance()
     {
-        distance = Math.round((Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y2,2)))*100)/100;
+        distance = Math.round(Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2))*100.0)/100.0;
         return distance;
     }
-
-    public String CalcY(double x)
+    //calculate the y value of x value by getting x as an argument and use the slop formula. Then reformat y and x into coordinate points
+    public String calcY(double x)
     {
-        double y = Math.round((slope*x + yInt)*100)/100;
+        double y = Math.round((slope*x + yInt)*100.0)/100.0;
         String coordinate = "(" + x +"," + y + ")";
         return coordinate;
     }
+    //return relevant information about the object
     public String toString()
     {
+        findSlope();
+        getYInt();
+        findFormula();
+        getDistance();
         String summary = "First Pair: " + coord1 + "\nSecond Pair: " + coord2 + "\n";
         summary += "Slope of line: " + slope + "\nY-intercept: " + yInt + "\n" ;
         summary += "Slope intercept form: " + formula + "\nDistance between points: " + distance;
